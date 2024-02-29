50dd317f9a944eaaaeffd340ab8f09bec98ad840
Yasufumi Misono <yasufumimisono@gmail.com>
2024-03-01 02:19:05 +0900

    edit models.py and admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import UserProfile, Restaurant, Review, Reservation, Like, RestaurantImages, BusinessHours
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Restaurant)
+admin.site.register(Review)
+admin.site.register(Reservation)
+admin.site.register(Like)
+admin.site.register(RestaurantImages)
+admin.site.register(BusinessHours)
+
+
